7bb77e2709024d2fac79ddf3362f44c0af42f3d6
vas <gringopiranha@gmail.com>
2023-04-17 08:59:10 -0700

    regex for email, minor ui change

--- js/user.js
+++ js/user.js
@@ -40,6 +40,7 @@ class User {
     this.ws_server = this.setup_socket(server);
   }
 
+  static email_regex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
   static port_min = 24101;
   static port_max = 25101;
   static current_port = 24101;
@@ -375,29 +376,43 @@ class User {
     let passw = params.get("password");
     let passw2 = params.get("password2");
     let email = params.get("email")
+    let register_err = false;
 
     // passwords don't match - error and try again
     if (passw != passw2) {
+      register_err = true;
       logger.error("registration error - " + "passwords do NOT match - please try again");
       response.writeHead(302 , {
           'Location' : '/?error_reg_pass'
       });
       response.end();
     }
-    else if (!email || email == "foo@bar.com") {
-      logger.error("registration error - " + "please enter valid email");
+    else if (!email) {
+      register_err = true;
+      logger.error("registration error - " + "please enter email");
+      response.writeHead(302 , {
+          'Location' : '/?error_reg_email'
+      });
+      response.end();
+    }
+    else if (!User.email_regex.test(email)) {
+      register_err = true;
+      logger.error("registration error - " + "please enter properly formatted email");
       response.writeHead(302 , {
           'Location' : '/?error_reg_email'
       });
       response.end();
     }
     else if (!display_name) {
+      register_err = true;
       logger.error("registration error - " + "please enter display name");
       response.writeHead(302 , {
           'Location' : '/?error_reg_display_name'
       });
       response.end();
     }
+
+    if (register_err) return;
 
     // check for existing user_name
     let dbq = { "user_name": user_name };
